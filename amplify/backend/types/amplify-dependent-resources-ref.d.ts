71b2c60984cf6da07b96268dfbf4d16cef68550b
Natalie <theliteshado@icloud.com>
2021-12-22 08:32:33 -0500

    first amplify push needs edits

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "natify": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
