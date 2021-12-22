f01ccd90d874b1c2e037035e46942fd6531004f3
Natalie <theliteshado@icloud.com>
2021-12-22 05:39:15 -0500

    player widget

--- App.tsx
+++ App.tsx
@@ -1,9 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import PlayerWidgets from './components/PlayerWidget';
+
+import Amplify from 'aws-amplify';
+import config from './aws-exports'
+
+Amplify.configure(config)
+
+import { AppContext } from './AppContext';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,8 +23,16 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
+        <AppContext.Provider
+        value={{
+          songId,
+          setSongId: (id: string) =>
+          setSongId(id),
+        }}>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
+        <PlayerWidgets />
+      </AppContext.Provider>
       </SafeAreaProvider>
     );
   }
